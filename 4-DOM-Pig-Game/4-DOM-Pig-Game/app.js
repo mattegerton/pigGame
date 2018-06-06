732676fb78162081c962f7d53d4761af187f418b
mattegerton <matthewjegerton@googlemail.com>
2018-06-06 07:41:54 +0100

    Additions to app.js: Dice now hides when page loads, all score elements set to 0, added random number rolling functionality and result is displayed using dice.png

--- 4-DOM-Pig-Game/4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/4-DOM-Pig-Game/app.js
@@ -8,3 +8,50 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+
+let scores, roundScore, activePlayer;
+
+score = [0,0]; // Counts total score.
+roundScore = 0; // Counts round score.
+activePlayer = 0; // Active player 0-1.
+
+
+// Hide the dice when page loads.
+document.querySelector('.dice').style.display = 'none';
+
+// Set elements score/current to 0.
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+function btn () {
+    //Do something here.
+};
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    
+    // 1. Produce a random number.
+    let dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display result.
+    let diceDOM = document.querySelector('.dice')
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    // 3. Display round result only if number is not a 1.
+
+} );
+
+
+
+
+
+
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '<em>';
+
+//let x = document.querySelector('#score-0').textContent;
+//console.log(x);
